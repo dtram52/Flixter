6f568b5597bced766fa3c280926bf0efd175f05d
Du Tram <du.tram90@gmail.com>
2020-10-04 04:01:50 -0700

    part 2 of Flixter updated

--- app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -1,19 +1,26 @@
 package com.example.flixter.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixter.DetailActivity;
 import com.example.flixter.R;
 import com.example.flixter.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -46,21 +53,22 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        RelativeLayout container;
         TextView tvTitle;
-        TextView tvOveview;
+        TextView tvOverview;
         ImageView ivPoster;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle =itemView.findViewById(R.id.tvTitle);
-            tvOveview = itemView.findViewById(R.id.tvOverview);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
-            tvOveview.setText(movie.getOverview());
+            tvOverview.setText(movie.getOverview());
             String imageUrl;
             //if phone is in landscape => imageURL=backdropImage
             if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
@@ -74,6 +82,18 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
 
             Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+            //1. Register click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //2. Navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    //i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie) );
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
